b220226281998cd5f7ce1db4379795cca24da92b
riyadh <riyadh02220@gmail.com>
2020-05-16 17:19:22 +0200

    game size is editable

--- app.js
+++ app.js
@@ -1,12 +1,67 @@
   // shapes :
-const path1 = document.getElementById("path4574");
-const path2 = document.getElementById("path4576");
-const circle = document.getElementById("path4578");
+const robeNballsContainer = document.querySelector(".balls");
+const increaseSize = document.getElementById("increase");
+const decreaseSize = document.getElementById("decrease");
+
+/* custom parameters*/
+let size = 4;
+let color = "grba()"
+
+/* size editing */
+
+increaseSize.addEventListener("click", ()=> {
+  if(size <= 12) {
+    size++ 
+    fill()
+  } else {
+    console.log("error");
+  }
+})
+
+decreaseSize.addEventListener("click", ()=> {
+  if(size >= 2) {
+    size--
+    fill()
+  } else {
+    console.log("error");
+  }
+})
+
+// initial function:
+const fill = ()=> {
+  robeNballsContainer.innerHTML = " "
+  /* add the first ball*/
+  robeNballsContainer.innerHTML += `
+      <div class="raw" id="left">
+        <div class="robe"></div>
+        <div class="ball"></div>
+      </div>
+    `
+  /*add the middle balls */
+  for (let i = 0; i < size; i++) {
+    robeNballsContainer.innerHTML += `
+      <div class="raw">
+          <div class="robe"></div>
+          <div class="ball"></div>
+        </div>
+      `
+  }
+
+  /* add the last ball*/
+  robeNballsContainer.innerHTML += `
+      <div class="raw" id="right">
+        <div class="robe"></div>
+        <div class="ball"></div>
+      </div>
+    `
+}
+/* fill the game with balls */
+fill()
 
 
-  // initial function:
 
 
+  /* interval 
   setInterval(() => {
 
 
@@ -17,5 +72,5 @@ const circle = document.getElementById("path4578");
 
 
   }, 4000);
-
+  */
 
