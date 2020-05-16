fa8ba8f466f1f190a0d2c57b00442de28e9d235f
riyadh <riyadh02220@gmail.com>
2020-05-16 18:47:46 +0200

    picking color button added

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
-  // shapes :
+  // elements :
 const robeNballsContainer = document.querySelector(".balls");
+let leftBall;
+let ball;
+let rightBall;
 const increaseSize = document.getElementById("increase");
 const decreaseSize = document.getElementById("decrease");
-
+const startButton = document.getElementById("start");
+const randomColor = document.getElementById("randomColor");
 /* custom parameters*/
 let size = 4;
 let color = "grba()"
+/* colors*/ 
+let clolors = [
+  "linear-gradient(45deg, #ff9a9e 0%, #fad0c4 99%, #fad0c4 100%)",
+  "linear-gradient(to top, #a18cd1 0%, #fbc2eb 100%)",
+  "linear-gradient(120deg, #f6d365 0%, #fda085 100%)",
+  "linear-gradient(to top, #fdcbf1 0%, #fdcbf1 1%, #e6dee9 100%)",
+  "linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%)",
+  "linear-gradient(to right, #43e97b 0%, #38f9d7 100%)",
+  "linear-gradient(to top, #5ee7df 0%, #b490ca 100%)",
+  "linear-gradient(to top, #d299c2 0%, #fef9d7 100%)",
+]
 
 /* size editing */
-
 increaseSize.addEventListener("click", ()=> {
-  if(size <= 12) {
+  if(size <= 9) {
     size++ 
     fill()
   } else {
@@ -27,7 +41,7 @@ decreaseSize.addEventListener("click", ()=> {
   }
 })
 
-// initial function:
+/* fill the game function*/
 const fill = ()=> {
   robeNballsContainer.innerHTML = " "
   /* add the first ball*/
@@ -54,23 +68,58 @@ const fill = ()=> {
         <div class="ball"></div>
       </div>
     `
+
+  leftBall = document.getElementById("left");
+  rightBall = document.getElementById("right");
+  ball = document.querySelectorAll(".ball");
 }
-/* fill the game with balls */
-fill()
 
-
-
-
-  /* interval 
+/* start game function */
+const startGame = ()=> {
+  // disable the button
   setInterval(() => {
 
+    leftBall.style.transform = "rotate(10deg)"
+    
+    setTimeout(() => {
+
+      leftBall.style.transform = "rotate(0deg)"
+      
+    }, 500);
 
     setTimeout(() => {
 
+      rightBall.style.transform = "rotate(-10deg)"
 
-    }, 2000);
+    }, 1000);
+
+    setTimeout(() => {
+
+      rightBall.style.transform = "rotate(0deg)"
+
+    }, 1500);
 
 
-  }, 4000);
-  */
+  }, 2000);
+}
+
+/* choose a random color */
+const pickColor = ()=> {
+      // choose a random namber:
+      let randomNumber = Math.random() * clolors.length
+      // display colors:
+    ball.forEach(bol => {
+      bol.style.backgroundImage = clolors[Math.floor(randomNumber)]
+    });
+}
+
+fill()
+startButton.addEventListener("click", startGame)
+randomColor.addEventListener("click", pickColor)
+
+
+
+    
+
+  
 
